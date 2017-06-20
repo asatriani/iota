57626e588bf4da7ef4742b5e226049b56f9929a8
satriani <satriani@ITMI00DW2050.corp.dom>
2017-06-20 18:05:06 +0200

    Fix json message

--- com.italtel.iota.demo.virtual_gas_meter/src/main/java/com/italtel/iota/demo/SendMeasureJob.java
+++ com.italtel.iota.demo.virtual_gas_meter/src/main/java/com/italtel/iota/demo/SendMeasureJob.java
@@ -3,7 +3,6 @@ package com.italtel.iota.demo;
 import java.util.Date;
 import java.util.Map;
 
-import org.eclipse.kura.message.KuraPayload;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -29,36 +28,26 @@ public class SendMeasureJob implements Job {
             Integer qos = (Integer) m_properties.get(VirtualGasMeter.PUBLISH_QOS_PROP_NAME);
             Boolean retain = (Boolean) m_properties.get(VirtualGasMeter.PUBLISH_RETAIN_PROP_NAME);
 
-            Date currentDate = new Date();
+            Long currentTimestamp = new Date().getTime();
             int meterNumber = virtualGasMeter.getMeasures().size();
             for (int i = 0; i < meterNumber; i++) {
-                String nameMeter = VirtualGasMeter.METER_PREFIX_NAME + i;
+                String meterName = VirtualGasMeter.METER_PREFIX_NAME + i;
                 double comsumption = virtualGasMeter.getRandom().nextDouble() * 4;
-                double measure = virtualGasMeter.getMeasures().get(nameMeter) + comsumption;
-                virtualGasMeter.getMeasures().put(nameMeter, measure);
+                double measure = virtualGasMeter.getMeasures().get(meterName) + comsumption;
+                virtualGasMeter.getMeasures().put(meterName, measure);
 
-                String finalTopic = new StringBuilder(nameMeter).append("/").append(topic).toString();
+                String finalTopic = new StringBuilder(meterName).append("/").append(topic).toString();
 
-                // Allocate a new payload
-                KuraPayload payload = new KuraPayload();
-
-                // Timestamp the message
-                payload.setTimestamp(currentDate);
-
-                // Add metric to the payload
-                payload.addMetric("value", measure);
-                payload.addMetric("meter", nameMeter);
-
-                byte[] body = payload.getBody();
-                String meterBody = new String(body);
-                s_logger.info("Published meterBody {}", meterBody);
+                String content = new StringBuilder("{").append("\"timestamp\": ").append(currentTimestamp)
+                        .append(", \"meter\": \"").append(meterName).append("\", \"measure\": ").append(measure)
+                        .append("}").toString();
 
                 // Publish the message
                 try {
-                    virtualGasMeter.getCloudClient().publish(finalTopic, payload, qos, retain);
-                    s_logger.info("Published message to {} for {}", finalTopic, nameMeter);
+                    virtualGasMeter.getCloudClient().publish(finalTopic, content.getBytes(), qos, retain, 5);
+                    s_logger.info("Published message to {} for {}", finalTopic, meterName);
                 } catch (Exception e) {
-                    s_logger.error("Cannot publish on topic {} for {}: {}", finalTopic, nameMeter, e.getMessage(), e);
+                    s_logger.error("Cannot publish on topic {} for {}: {}", finalTopic, meterName, e.getMessage(), e);
                 }
 
             }
